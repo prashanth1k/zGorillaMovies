3740211ec035a9075eeba95fa562151b90b9cd3e
Prashanth Krishnamurthy <prashnode@gmail.com>
2022-07-24 23:18:57 +0530

    added error handler

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/prashanth1k/zGorillaMovies
 
 go 1.18
 
-require github.com/gorilla/mux v1.8.0 // indirect
+require (
+	github.com/google/uuid v1.3.0
+	github.com/gorilla/mux v1.8.0 // indirect
+)
--- main.go
+++ main.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"crypto/rand"
+	"encoding/hex"
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
@@ -8,16 +11,78 @@ import (
 	"github.com/gorilla/mux"
 )
 
+type Movie struct {
+	ID       string    `json:"id"`
+	Title    string    `json:"title"`
+	Director *Director `json:"director"`
+}
+type Director struct {
+	ID   string `json:"id"`
+	Name string `json:"name"`
+}
+
+var movies []Movie
+
 func main() {
-	router := mux.NewRouter()
 
-	router.HandleFunc("/movies/", getMovies).Methods("GET")
-	router.HandleFunc("/movies/id", getMovie).Methods("GET")
-	router.HandleFunc("/movies/", postMovie).Methods("POST")
-	router.HandleFunc("/movies/", putMovie).Methods("PUT")
-	router.HandleFunc("/movies/", delMovie).Methods("DELETE")
+	r := mux.NewRouter()
 
-	fmt.Printf("String server at Port 5000")
-	log.Fatal(http.ListenAndServe(":5000", router))
+	r.HandleFunc("/movies", getMovies).Methods("GET")
+	r.HandleFunc("/movies/id", getMovie).Methods("GET")
+	r.HandleFunc("/movies", postMovie).Methods("POST")
+	r.HandleFunc("/movies", deleteMovie).Methods("DELETE")
+	r.NotFoundHandler = r.NewRoute().HandlerFunc(customErrorHandler).GetHandler()
 
+	fmt.Println("Starting server at Port 5000..\n")
+	log.Fatal(http.ListenAndServe(":5000", r))
+
+}
+
+func getMovies(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(movies)
+}
+
+func getMovie(w http.ResponseWriter, r *http.Request) {
+	fmt.Printf("GetMovie..")
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	for _, item := range movies {
+		if item.ID == params["id"] {
+			json.NewEncoder(w).Encode(item)
+			return
+		}
+	}
+
+}
+func postMovie(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	var movie Movie
+	_ = json.NewDecoder(r.Body).Decode(&movie)
+
+	// generate random, hopefully unique string
+	randId := make([]byte, 4)
+	rand.Read(randId)
+
+	movie.ID = hex.EncodeToString(randId)
+	movies = append(movies, movie)
+	json.NewEncoder(w).Encode(movie)
+}
+
+func deleteMovie(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	movie := movies[0]
+	for index, item := range movies {
+		if item.ID == params["id"] {
+			movie = movies[index]
+			movies = append(movies[:index], movies[index+1:]...)
+			break
+		}
+	}
+	json.NewEncoder(w).Encode(movie)
+}
+
+func customErrorHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Not handled")
 }
